607f75f7656f827eee9ae2b96f67aa11bd84d85f
KauanAntunesLima <kauanantuneslima07@gmail.com>
2025-08-23 22:33:02 -0300

    atualizado

--- main.js
+++ main.js
@@ -2,10 +2,19 @@
 
 function criarImagem(urlImagem){
     const galeria = document.getElementById('galeria')
+
     const img = document.createElement('img')
+
+     const card = document.createElement('div')
+
+    card.classList.add('card')
+
     img.src = urlImagem.url
 
-    galeria.appendChild(img)
+    card.style.setProperty('--legenda', `"${urlImagem.subtitle}"`)
+
+    card.appendChild(img)
+    galeria.appendChild(card)
     
 }
 
@@ -14,51 +23,61 @@ function carregarImagens(){
        
         {
             nome: 'cell',
+            subtitle: 'Cell',
             url: './img/cell.png'
         },
 
         {
             nome: 'freeza',
+            subtitle: 'Freeza',
             url: './img/freeza.png'
         },
 
         {
             nome: 'gohan',
+            subtitle: 'Gohan',
             url: './img/gohan.png'
         },
 
         {
             nome: 'goku black',
+            subtitle: 'Goku black',
             url: './img/goku black.png'
         },
 
         {
             nome: 'goku',
+            subtitle: 'Goku',
             url: './img/goku.png'
         },
 
         {
             nome: 'jiren',
+            subtitle: 'Jiren',
             url: './img/jiren.png'
         },
 
         {
             nome: 'majin boo',
+            subtitle: 'Majin boo',
             url: './img/majin boo.jpg'
         },
 
         {
             nome: 'piccolo',
+            subtitle: 'Piccolo',
             url: './img/piccolo.png'
         },
 
         {
             nome: 'trunks',
+            subtitle: 'Trunks',
             url: './img/trunks.png'
         },
 
         {
             nome: 'vegeta',
+            subtitle: 'Vegeta',
             url: './img/vegeta.png'
         }
     ]
